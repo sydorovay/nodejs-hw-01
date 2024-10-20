89dfdca6718d1b3ec099573188cf9c2bbb18e272
sydorovay <sydorovay@gmail.com>
2024-10-20 18:41:48 +0200

    Update readContacts.js

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { readFile } from 'fs/promises';
+import { PATH_DB } from '../constants/contacts';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    // Зчитуємо файл з вказаного шляху
+    const data = await readFile(PATH_DB, 'utf-8');
+    // Парсимо дані JSON
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error('Error reading contacts:', error); // Логування помилки
+    throw new Error('Unable to read contacts'); // Генерація нової помилки
+  }
+};
