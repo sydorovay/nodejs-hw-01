5093cdda4635a23618dfa08735feb897b398c5c4
sydorovay <sydorovay@gmail.com>
2024-10-20 20:56:59 +0200

    Update writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,15 @@
+import { writeFile } from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    // Конвертація оновлених контактів у формат JSON
+    const data = JSON.stringify(updatedContacts, null, 2); // Додавання форматування для зручності
+    // Запису даних у файл
+    await writeFile(PATH_DB, data, 'utf-8');
+    console.log("Contats have been sucessfuly written to the file.");
+  } catch (error) {
+    console.error("Error writing contacts:", error); // Логування помилки
+    throw new Error("Unable to write contacts"); // Генерація нової помилки
+  }
+};
